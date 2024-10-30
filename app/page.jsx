0a7490f00f7e8f7a3bdc00929d7ae364e307aaab
Vladimir <ursatiivladimir@gmail.com>
2024-10-30 20:47:07 +0300

    fix D btn

--- app/page.jsx
+++ app/page.jsx
@@ -3,15 +3,17 @@ import {FiDownload} from 'react-icons/fi'
 //component
 import Socials from "@/components/Socials";
 import Photo from "@/components/Photo";
-import Stats from "@/components/Stats"
+import Stats from "@/components/Stats";
+
+document.getElementById("downloadBtn").addEventListener("click", function() {
+    const link = document.createElement("a");
+    link.href = "https://drive.usercontent.google.com/u/0/uc?id=1XkE2AUL63jTVXHlnIykLNVxjGtkF5uX6&export=download";
+    link.download = "CV_UrsatiiVladimir.pdf";
+    link.click();
+});
 
 const Home = () => {
-    document.getElementById("downloadBtn").addEventListener("click", function() {
-        const link = document.createElement("a");
-        link.href = "https://drive.usercontent.google.com/u/0/uc?id=1XkE2AUL63jTVXHlnIykLNVxjGtkF5uX6&export=download";
-        link.download = "CV_UrsatiiVladimir.pdf";
-        link.click();
-    });
+
   return (
       <section className="h-full">
           <div className="container mx-auto h-full">
